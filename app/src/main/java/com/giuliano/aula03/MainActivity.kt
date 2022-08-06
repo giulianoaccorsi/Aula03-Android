f6fe9cef9be51bd636b73410fc282174c6f6b521
Giuliano Accorsi <giulianoaccorsi@gmail.com>
2022-08-06 12:14:50 -0300

    :art: Improve structure / format of the code.

--- app/src/main/java/com/giuliano/aula03/MainActivity.kt
+++ app/src/main/java/com/giuliano/aula03/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.giuliano.aula03
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Procura o botão pelo ID
         val rodarNumero = findViewById<Button>(R.id.btn_rodar)
+        val numeroDigitado = findViewById<EditText>(R.id.editTextNumberSigned)
 
-        // Ao clicar no botão, procura o TextView pelo ID e adiciona um número sorteado a ele.
         rodarNumero.setOnClickListener {
-            val texto = findViewById<TextView>(R.id.number_sorted)
-            val numeroSorteado = Random.nextInt(11)
-            val textDisplayed = "Número sorteado é: $numeroSorteado"
-            texto.text = textDisplayed
+            val numero = numeroDigitado.text.toString().toInt()
+            if(numero >= 0 && numero <= 10) {
+                val numeroSorteado = Random.nextInt(11)
+                if (numeroSorteado == numero) {
+                    Toast.makeText(this@MainActivity, "Parabéns você foi sorteado !!!!", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this@MainActivity, "Não foi dessa vez :(", Toast.LENGTH_SHORT).show()
+                }
+            }else {
+                Toast.makeText(this@MainActivity, "Digite um número de 0 até 10", Toast.LENGTH_SHORT).show()
+            }
+
+
         }
     }
 }
